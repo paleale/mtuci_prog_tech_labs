84ea2e5fecb4eeb5ce730f2f492cdc4c119483a0
Alena Maslova <amaslova@iponweb.net>
2020-11-28 16:58:48 +0300

    Add Lab2; do some things of Lab1 public

--- src/ru/mtuci/progtech/Lab1.java
+++ src/ru/mtuci/progtech/Lab1.java
@@ -4,25 +4,27 @@ import java.lang.Math;
 import java.lang.System;
 
 public class Lab1 {
-    static double A=-5;
-    static double B=5;
+    public static double A=-5;
+    public static double B=5;
+
     static double H=0.1;
     static double x, y;
     static double sum = 0;
+    // M = any value
     static int M = 34;
 
-    /* Метод F() вычисляет значение функции f(x) = xe^x + 2sin(x) - sqrt(|x^3 - x^2|) */
-    static double F(double x) {
+    // Метод F() вычисляет значение функции f(x) = xe^x + 2sin(x) - sqrt(|x^3 - x^2|)
+    public static double F(double x) {
         return x * Math.pow(Math.exp(x), x) + 2 * Math.sin(x) - Math.sqrt(Math.abs(Math.pow(x, 3) - Math.pow(x, 2)));
     }
 
-    /* Основной метод */
+    // Основной метод
     public static void main(String[] args) {
-        System.out.printf("Аргумент  Значение функции\n");
+        System.out.println("Аргумент  Значение функции");
         for (x = A; x <= B; x += H) {
             y = F(x);
             System.out.printf("%10.4f %10.4f\n", x, y);
-            /* Условие */
+            // Условие
             if (Math.abs(y) < M)
             {
                 sum += Math.abs(y);
---
+++ src/ru/mtuci/progtech/Lab2.java
@@ -0,0 +1,34 @@
+package ru.mtuci.progtech;
+
+import java.io.*;
+
+public class Lab2 {
+    static double x, y, h;
+    static int n;
+    static int count=0;
+    // A17. (-6, -10), (-6, 3), (4, 3), (4, -10)
+    // Вершины:
+    static int P1=-6;
+    static int P2=4;
+    static int P3=-10;
+    static int P4=3;
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.println("Enter n: ");
+            n = Integer.parseInt(br.readLine());
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        h=(Lab1.B - Lab1.A) / (n - 1);
+        for(x = Lab1.A; x <= Lab1.B; x += h) {
+            y=Lab1.F(x);
+            System.out.printf("%10.4f %10.4f\n", x, y);
+            if (x >= P1 && x <= P2 && y >= P3 && y <= P4) {
+                count++;
+            }
+        }
+        System.out.printf("count = %d\n", count);
+    }
+}
